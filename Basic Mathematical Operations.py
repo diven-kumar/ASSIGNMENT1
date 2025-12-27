b2bed5b3855e844d8edd0cbc8e8e0a40eedb3989
diven-kumar <divenkumar2007@gmail.com>
2025-12-27 12:23:17 +0530

    Add files via upload

---
+++ Basic Mathematical Operations.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter the first number :"))
+num2=int(input("Enter the second number :"))
+Addition=num1+num2
+subraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("Addition=",Addition)
+print("Subtraction=",subraction)
+print("Multiplication=",multiplication)
+print("Division=",division)
+
---
+++ Personalized Greeting.py
@@ -0,0 +1,6 @@
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+Full_name = First_name + Last_name
+first_greeting=("Hello,")
+last_Greeting=("Welcome to the python program")
+print(first_greeting,(Full_name)+"!",last_Greeting)
